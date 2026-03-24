beb698607d11509883114d82d28b8b09e3266bcf
Shavindi Perera <w.shavindiperera@gmail.com>
2026-03-24 22:14:41 +0530

    Created new hero sections and updated homepage ui

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -5,20 +5,22 @@
 "use client";
 
 import { AboutSection } from '@/components/about/about';
-import { About } from "@/components/home/about-section";
+import HomeAboutSection from "@/components/home/HomeAboutSection";
+import GlobeSection from "@/components/home/globe-section";
 import { Brands } from "@/components/home/brands";
 import Domains from "@/components/home/domains";
-import Hero from "@/components/home/hero";
 import ImpactStats from "@/components/home/impact-stats";
 import { LanguageProvider } from "@/hooks/LanguageProvider";
 import LanguageToggle from "@/components/LanguageToggle";
+import NewHero from "@/components/NewHero";
 
 export default function Home() {
   return (
     <LanguageProvider>
       <div className="flex flex-col gap-12 pb-12">
-        <Hero />
-        <About />
+        <NewHero />
+        <HomeAboutSection />
+        <GlobeSection />
         <AboutSection/>
         <Domains />
         <ImpactStats />
