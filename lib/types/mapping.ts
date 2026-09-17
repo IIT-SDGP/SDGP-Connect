0b941ddefcf12da852c376fd45df75021d74df5e
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2026-09-17 10:40:14 +0530

    Update mapping.ts

--- lib/types/mapping.ts
+++ lib/types/mapping.ts
@@ -276,5 +276,6 @@ export const sdgGoals = [
       { value: "I-25-jan", label: "InfoSchol 2025 Jan" ,type: "Infoschol" },
       { value: "I-25-sep", label: "InfoSchol 2025 Sep" ,type: "Infoschol" },
       { value: "I-26-jan", label: "InfoSchol 2026 Jan" ,type: "Infoschol" },
+      { value: "I-27-jan", label: "InfoSchol 2027 Jan" ,type: "Infoschol" },
       { value: "VisioNEX-2025", label: "VisioNEX 2025" ,type: "Visionex" },
     ]
